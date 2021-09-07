59b0731510098c0f1a65356c76697cae0c7833b5
Millie <24589518+millieg@users.noreply.github.com>
2021-09-07 18:59:29 +0100

    Update config.js

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
     // Service name used in header. Eg: 'Renew your passport'
-    serviceName: 'IPAFFS',
+    serviceName: 'Import of products, animals, food and feed service',
 
     // Default port that prototype runs on
     port: '3000',
