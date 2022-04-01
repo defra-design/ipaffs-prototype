089b54c38252c1d339c3562159d4810d6b446925
Millie <24589518+millieg@users.noreply.github.com>
2022-04-01 14:00:57 +0100

    index page and file upload

--- app/routes.js
+++ app/routes.js
@@ -1,10 +1,14 @@
 const express = require('express')
 const router = express.Router()
 
+
+
 // Add your routes here - above the module.exports line
 
 
 // Start folder specific route
 router.use('/v1', require('./views/v1/_routes'))
+
+router.use('/create-cheda', require('./views/create-cheda/_routes'))
 
 module.exports = router
