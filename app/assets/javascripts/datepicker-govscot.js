6a2c2720920203110a85c857603cf9e4b28dc8cb
Marc-Steeven.Eyeni-Kantsey <marc-steeven.eyeni-kantsey@defra.onmicrosoft.com>
2022-07-07 14:43:09 +0100

    Implemented gov scot date picker

---
+++ app/assets/javascripts/datepicker-govscot.js
@@ -0,0 +1,9 @@
+// var scotInput = document.querySelector('#scot-date-picker');
+var dsDatepickers = document.querySelectorAll('[data-module="ds-datepicker"]');
+
+$(document).ready(function () {
+  const options = {
+    imagePath: '/public/images/icons/',
+  }
+  dsDatepickers.forEach(datePicker => new window.DS.components.DSDatePicker(datePicker, options).init());
+})
