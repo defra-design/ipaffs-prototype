d013a8648c0dc597e139df2177697e62eab778eb
Millie <24589518+millieg@users.noreply.github.com>
2022-07-08 15:58:21 +0100

    ignore

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -2,10 +2,9 @@
 
 // Warn about using the kit in production
 if (window.console && window.console.info) {
-    window.console.info('GOV.UK Prototype Kit - do not use for production')
+  window.console.info('GOV.UK Prototype Kit - do not use for production')
 }
 
-$(document).ready(function() {
-    window.GOVUKFrontend.initAll();
-
+$(document).ready(function () {
+  window.GOVUKFrontend.initAll()
 })
