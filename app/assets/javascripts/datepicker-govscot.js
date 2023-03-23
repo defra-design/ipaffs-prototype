f58461c228edaf336de8406de2360a18a7251575
Millie Guest <24589518+millieg@users.noreply.github.com>
2023-03-23 23:49:53 +0000

    CHED A to org page

--- app/assets/javascripts/datepicker-govscot.js
+++
@@ -1,8 +0,0 @@
-var dsDatepickers = document.querySelectorAll('[data-module="ds-datepicker"]');
-
-$(document).ready(function () {
-  const options = {
-    imagePath: '/public/images/icons/',
-  }
-  dsDatepickers.forEach(datePicker => new window.DS.components.DSDatePicker(datePicker, options).init());
-})
