60a33e58c19c40e41279b94b24db39983824d583
JanaL-boop <j.latzberg@outlook.com>
2023-07-27 11:00:12 +0100

    Add imp to chedd script

--- app/assets/javascripts/imptocheddsccript.js
+++ app/assets/javascripts/imptocheddsccript.js
@@ -6,23 +6,29 @@ $( document ).ready(function() {
     }
   }); */
 
-  if (document.referrer == "http://localhost:3000/breakfast/checkanswers") {
-    $('form').attr('action','http://localhost:3000/breakfast/checkanswers')
+  //go to check-answers page (hub) when 'Save and return to hub' button is clicked on any page
+  $('#save-and-return-button').click(function (){
+    $('form').attr('action','/IMPtoCHEDD/check-answers')
+  })
+
+  // when a page is openened from the review page, it will show the 'Save and review button' instead
+  // of the usual three buttons, and it will link back to the review page
+  if (document.referrer == "http://localhost:3000/IMPtoCHEDD/review") {
+    $(".save-and-submit-buttons").html("<input class=\"govuk-button govuk-!-margin-right-1 govuk-!-margin-bottom-4\" id=\"save-and-review-button\" name=\"save-and-review-button\" type=\"submit\" value=\"Save and review\" data-module=\"govuk-button\">");
+    $('form').attr('action','http://localhost:3000/IMPtoCHEDD/review')
   }
 
-  $('input:radio[name=breakfasttype]').change(function(){
-    var option = $(this).val();
+  // automatically checks the 'for internal market' option
+  if(document.URL === 'http://localhost:3000/IMPtoCHEDD/reason-for-import') {
+    $('#are-consignments-in-containers-no').prop('checked',true);
+    $('#conditional-for-transfer-to-dpe').hide()
+  }
+// when 'for internal market' option is clicked the expandable drop-down is hidden and vice versa
+  $('#are-consignments-in-containers-yes').click(function (){
+    $('#conditional-for-transfer-to-dpe').show()
+  })
 
-
-    if (option == 'Standard'){
-      $('form').attr('action', 'standard-options');
-    }
-    if (option == 'Vegetarian'){
-      $('form').attr('action', 'veggie-options');
-    }
-    if (option == 'Vegan'){
-      $('form').attr('action', 'vegan-options');
-    }
-  });
-
+  $('#are-consignments-in-containers-no').click(function (){
+    $('#conditional-for-transfer-to-dpe').hide()
+  })
 });
