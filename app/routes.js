f770559d0c9745823923b977ab65b7bb8e34d1ce
Millie Guest <24589518+millieg@users.noreply.github.com>
2023-08-31 19:01:23 +0100

    iuu page notices

--- app/routes.js
+++ app/routes.js
@@ -12,3 +12,22 @@ router.use('/v1', require('./views/v1/_routes'))
 router.use('/create-cheda', require('./views/create-cheda/_routes'))
 
 module.exports = router
+
+
+
+// Run this code when a form is submitted to 'juggling-balls-answer'
+router.post('/part-2/iuu/06-laboratory', function (req, res) {
+
+    // Make a variable and give it the value from 'how-many-balls'
+    var laboratoryTests = req.session.data['laboratory-tests']
+  
+    // Check whether the variable matches a condition
+    if (laboratoryTests == "Yes"){
+      // Send user to next page
+      res.redirect('/part-2/iuu/06a-laboratory')
+    } else {
+      // Send user to ineligible page
+      res.redirect('/part-2/iuu/07a-decision')
+    }
+  
+  })
