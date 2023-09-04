6e4243020b4f7e168056008751646602151e7a40
Millie Guest <24589518+millieg@users.noreply.github.com>
2023-09-04 18:08:47 +0100

    catch cert pages

--- app/routes.js
+++ app/routes.js
@@ -31,3 +31,59 @@ router.post('/laboratory-test', function (req, res) {
     }
   
   })
+
+
+
+// Part 1 iuu accompanying docs page
+router.post('/iuu-doc-route', function (req, res) {
+
+  // Make a variable and give it the value from 'how-many-balls'
+  var iuuDocRoute = req.session.data['iuuDocType']
+
+  
+
+  // Check whether the variable matches a condition
+  if (iuuDocRoute == "Catch certificates"){
+    // Send user to next page
+     // Store the value in localStorage
+
+    res.redirect('/part-1-chedp/iuu/09aa-multi-upload-catch')
+  }
+  else if (iuuDocRoute == "catchcert1") {
+    // Send user to ineligible page
+    res.redirect('/part-1-chedp/iuu/09-iuu-docs-1entered')
+  }
+  
+  else if (iuuDocRoute == "Processing statements") {
+    // Send user to ineligible page
+    res.redirect('/part-1-chedp/iuu/09a-multi-upload')
+  }
+
+  else if (iuuDocRoute == "processingstatement1") {
+    // Send user to ineligible page
+    res.redirect('/part-1-chedp/iuu/09-iuu-docs-1entered')
+  }
+
+ else if (iuuDocRoute == "Storage statements") {
+  // Send user to ineligible page
+  res.redirect('/part-1-chedp/iuu/09a-multi-upload')
+}
+
+else if (iuuDocRoute == "storagedoc1") {
+  // Send user to ineligible page
+  res.redirect('/part-1-chedp/iuu/09-iuu-docs-1entered')
+}
+
+else if (iuuDocRoute == "Save and return to task list") {
+  // Send user to ineligible page
+  res.redirect('/part-1-chedp/iuu/07-check-answers')
+}
+
+
+ else   {
+  // Send user to ineligible page
+  res.redirect('/part-1-chedp/iuu/09e-iuu-confirmation')
+}
+
+})
+
