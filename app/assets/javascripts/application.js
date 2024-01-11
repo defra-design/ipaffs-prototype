bcb8bf9daf26973bfed6ceb66d9523135b035c8a
Millie Guest <24589518+millieg@users.noreply.github.com>
2024-01-11 17:51:16 +0000

    CHEDP hiding and showing reasons

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -7,4 +7,23 @@ if (window.console && window.console.info) {
 
 $(document).ready(function () {
   window.GOVUKFrontend.initAll()
-})
+
+
+  $('input:radio[name=regulationConformQ]').change(function () {
+    var optionSelected = $(this).val();
+    if (optionSelected == "yes") {
+        //alert('Works yes');
+        $("#purpose").removeClass("hidden-reason");
+        $("#purpose-nc").addClass("hidden-reason");
+        $("#conform-hint").addClass("hidden-reason");
+    }
+
+    if (optionSelected == "no") {
+        //alert('Works no');
+        $("#purpose-nc").removeClass("hidden-reason");
+        $("#purpose").addClass("hidden-reason");
+        $("#conform-hint").addClass("hidden-reason");
+    }
+});
+   
+});
