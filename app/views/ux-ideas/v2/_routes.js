cf0c6b79c51d8c934edfb807f6a3c735855414dd
grahamri <39337684+grahamri@users.noreply.github.com>
2024-05-23 08:32:34 +0100

    Remove UX-ideas folder

--- app/views/ux-ideas/v2/_routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const router = express.Router()
-
-// Add your routes here - above the module.exports line
-
-// console.log( 'This is the V1 _routes file' );
-
-module.exports = router
