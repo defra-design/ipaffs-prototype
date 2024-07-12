c468ca545db4610cf39e1c1b67ecfbb2a3e3c710
Bogdan Baraskevic <b.baraskevic@kainos.com>
2024-07-13 01:02:42 +0200

    SINGLE CHED-A: trying to add same commodity twice scenario

--- app/views/cheda/single-cheda/latest/part1/_routes.js
+++ app/views/cheda/single-cheda/latest/part1/_routes.js
@@ -1,10 +1,16 @@
 const express = require('express')
 const router = express.Router()
 
-
+//  02-01-commodity-tree-code-search
+router.post('/commodity-code-validation', function (req, res) {
+  var commodityCode = req.session.data['commodity-code']
+  if (commodityCode == "01061900") {
+    res.redirect('02-02a-commodity-tree-code-search-expanded-error')
+  }
+  else {
+    res.redirect('02-03-commodity-species')
+  }
+})
   
  
-  
-  
-  
-  module.exports = router
+module.exports = router
