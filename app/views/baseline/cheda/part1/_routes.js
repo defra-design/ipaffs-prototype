f58d96392ff7dd5d66f7a5fef25517970045c162
Bogdan Baraskevic <b.baraskevic@kainos.com>
2024-09-11 16:55:52 +0200

    BASELINE CHED-A: applied changes from SINGLE CHED-A theme

--- app/views/baseline/cheda/part1/_routes.js
+++ app/views/baseline/cheda/part1/_routes.js
@@ -1,18 +1,7 @@
 const express = require('express')
 const router = express.Router()
 
-//  02-01-commodity-tree-code-search
-router.post('/commodity-code-validation', function (req, res) {
-  var commodityCode = req.session.data['commodity-code']
-  if (commodityCode == "01061900") {
-    res.redirect('02-01a-commodity-tree-code-search-error')
-  }
-  else {
-    res.redirect('02-03-commodity-species')
-  }
-})
-
-//  05-01-transport-to-poe
+/*  05-01-transport-to-poe
 router.post('/means-of-transport', function (req, res) {
   var transportMeans = req.session.data['transport-means-before']
   if (transportMeans == "Airplane") {
@@ -21,7 +10,7 @@ router.post('/means-of-transport', function (req, res) {
   else {
     res.redirect('05-03-goods-movement-services')
   }
-})
+}) */
   
  
 module.exports = router
